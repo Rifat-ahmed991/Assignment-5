1636efc8ffe01d67b66a89d6f032afbd781af81e
Rifat ahmed <rifatahmed991@gmail.com>
2021-02-08 09:31:38 +0600

    hoyni

--- app.js
+++ app.js
@@ -3,18 +3,23 @@ fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
 .then(res => res.json())
 .then(data => displayFoods(data.categories));
 
-
-
 const displayFoods = foods => {
-    const ul = document.getElementById('foodList');
+    
+     const foodListDiv = document.getElementById('foodList');
     for (let i = 0; i< foods.length; i++) {
       const food = foods[i];
-       const li = document.createElement('li');
-       li.innerHTML = foods.idCategories;
-       ul.appendChild(li); 
+        const foodDiv = document.createElement('div');
+        foodDiv.className = 'food';
+        const foodInfo = `
+            <img src=${food.strCategoryThumb}>
+            <h3 class="food-name">${food.strCategory}</h3>   
+        `
+        foodDiv.innerHTML = foodInfo;
+        foodListDiv.appendChild(foodDiv);
        
     }
 
 }
+
 
 
